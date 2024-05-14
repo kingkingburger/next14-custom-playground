0cfa5dd6a79a83224f2af250997e2440f227d714
kingkingburger <dnjsalsgh123@gmail.com>
2024-05-14 17:25:03 +0900

    update: 게시판 등록하는 화면 만들기

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -2,14 +2,95 @@
 
 import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-const schema = z.object({
+const formSchema = z.object({
   title: z.string().min(5, { message: "제목은 최소 5글자가 필요해요" }),
   content: z.string().min(10, { message: "내용은 최소 10글자가 필요해요" }),
 });
 
-type FormData = z.infer<typeof schema>;
+type FormData = z.infer<typeof formSchema>;
 
 const NewPost = () => {
-  useForm<FormData>({ resolver: zodResolver(schema) });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // toast(`카드가 생성되지 않았습니다.`, {
+    //   description: "카드 생성 에러",
+    //   action: {
+    //     label: "확인",
+    //     onClick: () => console.log("Undo"),
+    //   },
+    // });
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-6">새로운 글</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>제목</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="제목을 입력해주세요"
+                    className="border-4"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>내용</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="내용을 작성해주세요"
+                    className="border-4 h-80"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
+
+export default NewPost;
