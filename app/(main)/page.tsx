710151a5f1b07008d6c3096403beee7e0f04d695
kingkingburger <dnjsalsgh123@gmail.com>
2024-06-14 10:29:23 +0900

    update: network call 확인 로그 생성

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,8 @@
+// page.tsx
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import usePostStore from "@/store/postStore";
 import LoadingSpinner from "@/components/loading-spinner";
 
@@ -10,20 +11,26 @@ export interface PostType {
   title: string;
   content: string;
 }
-
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const { fetchPosts, postList } = usePostStore();
+  const hasFetchedData = useRef(false); // useRef로 fetchData 호출 상태를 저장
 
   useEffect(() => {
+    console.log("useEffect called");
+
+    if (hasFetchedData.current) return; // fetchData가 이미 호출되었으면 종료
+    hasFetchedData.current = true; // fetchData 호출 상태 업데이트
+
     const fetchData = async () => {
+      console.log("fetchData called");
       setIsLoading(true);
       try {
         await fetchPosts();
         setIsClient(true);
       } catch (error) {
-        console.error("Failed to fetch posts:", error);
+        console.error("Failed to fetch posts:", error.message);
       } finally {
         setIsLoading(false);
       }
@@ -36,7 +43,6 @@ export default function Home() {
     return <LoadingSpinner />; // 로딩 중일 때 표시할 내용
   }
 
-  // postList가 PostType[] 타입임을 명시
   const typedPostList = postList as PostType[];
 
   return (
