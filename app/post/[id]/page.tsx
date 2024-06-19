bec7f13ac24ae6be2736baafec144c5e79a7e01f
kingkingburger <dnjsalsgh123@gmail.com>
2024-06-19 12:07:30 +0900

    update: change post info page to ssr

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,37 +1,27 @@
-"use client";
+import ApiService from "@/lib/fetch";
 
-import React, { useEffect, useState } from "react";
-import CommentPage from "@/components/comment/comment";
-import usePostStore from "@/store/postStore";
-import { useParams } from "next/navigation";
+interface PostIdPageProps {
+  params: {
+    id: string;
+  };
+}
 
-export default function PostIdPageClient() {
-  const [isClient, setIsClient] = useState(false);
-  const params = useParams();
-  const { getPost, selectPost } = usePostStore();
-
-  useEffect(() => {
-    setIsClient(true);
-    getPost(params?.id as string);
-  }, []);
-
-  if (!isClient) {
-    return null; // 클라이언트에서만 랜더링할 내용이 있다면 여기를 사용
-  }
+export default async function PostIdPageClient({ params }: PostIdPageProps) {
+  const apiService = new ApiService();
+  const postResult = await apiService.getPostById(params.id);
+  const post = postResult.data;
 
   return (
     <div className="bg-gray-900 min-h-screen p-4">
       <main className="max-w-3xl mx-auto p-6 bg-gray-800 rounded-lg shadow-lg mt-10 border border-gray-700">
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          {selectPost?.title}
-        </h1>
+        <h1 className="text-4xl font-bold mb-4 text-white">{post?.title}</h1>
         <div className="flex justify-between items-center mb-4 text-gray-400">
-          <span>ID: {selectPost?.id}</span>
-          <span>{selectPost?.createdDate}</span>
+          <span>ID: {post?.id}</span>
+          <span>{post?.createdDate}</span>
         </div>
-        <div className="text-lg text-gray-300">{selectPost?.content}</div>
+        <div className="text-lg text-gray-300">{post?.content}</div>
       </main>
-      <CommentPage />
+      {/*<CommentPage />*/}
     </div>
   );
 }
--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,9 +1,10 @@
 import { FormData } from "@/app/new-post/page";
+import { revalidatePath, revalidateTag } from "next/cache";
 
-interface PostType {
+export interface PostResult<T> {
   message: string;
   code: number;
-  data: PostData | PostData[];
+  data: T;
 }
 
 export interface PostData {
@@ -20,7 +21,7 @@ class ApiService {
     this.serverUrl = process.env.NEXT_PUBLIC_SERVER || "";
   }
 
-  public async fetchPosts(): Promise<PostData[]> {
+  public async fetchPosts(): Promise<PostResult<PostData[]>> {
     try {
       const response = await fetch(`${this.serverUrl}/api/post`, {
         method: "GET",
@@ -28,14 +29,18 @@ class ApiService {
           "Content-Type": "application/json",
           // Authorization: `Bearer ${token}`,
         },
+        cache: "no-cache",
+        next: {
+          // revalidate: 2,
+          // tags: ["main"],
+        },
       });
 
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
 
-      const data = await response.json();
-      return data;
+      return await response.json();
     } catch (error: any) {
       console.error("Failed to fetch posts:", error.message);
       throw error;
@@ -62,6 +67,32 @@ class ApiService {
 
       const data = await response.json();
       return data;
+    } catch (error: any) {
+      console.error("Failed to fetch posts:", error.message);
+      throw error;
+    }
+  }
+
+  public async getPostById(id: string): Promise<PostResult<PostData>> {
+    try {
+      const response = await fetch(`${this.serverUrl}/api/post/id/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${token}`,
+        },
+        cache: "no-cache",
+        next: {
+          // revalidate: 2,
+          // tags: ["main"],
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      return await response.json();
     } catch (error: any) {
       console.error("Failed to fetch posts:", error.message);
       throw error;
