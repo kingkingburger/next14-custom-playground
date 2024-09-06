924e763b0c337e4a8cd40784f581fa6896606c50
minho <dnjsalsgh123@gmail.com>
2024-09-06 20:32:23 +0900

    [UPDATE]: setup like button

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -28,6 +28,7 @@ export default async function PostIdPageClient({ params }: PostIdPageProps) {
     <div className="bg-gray-900 min-h-screen p-4">
       <main className="max-w-3xl mx-auto p-6 bg-gray-800 rounded-lg shadow-lg mt-10 border border-gray-700">
         <h1 className="text-4xl font-bold mb-4 text-white">{post?.title}</h1>
+        <button>좋아요</button>
         <div className="flex justify-between items-center mb-4 text-gray-400">
           <span>ID: {post?.User.name}</span>
 
