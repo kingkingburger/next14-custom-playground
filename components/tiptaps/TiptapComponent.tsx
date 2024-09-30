22cb180f0acd084d73040eedd14acc3b02ef58fb
minho <dnjsalsgh123@gmail.com>
2024-10-01 04:58:18 +0900

    [UPDATE]: size up the editorContent

--- components/tiptaps/TiptapComponent.tsx
+++ components/tiptaps/TiptapComponent.tsx
@@ -16,6 +16,7 @@ const TiptapComponent = ({ onChange, content }: any) => {
       attributes: {
         class:
           "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+        style: "min-height: 400px;",
       },
     },
     onUpdate: ({ editor }) => {
@@ -27,7 +28,10 @@ const TiptapComponent = ({ onChange, content }: any) => {
     <div className="w-full px-4">
       <Toolbar editor={editor} content={content} />
 
-      <EditorContent style={{ whiteSpace: "pre-line" }} editor={editor} />
+      <EditorContent
+        style={{ whiteSpace: "pre-line", minHeight: "400px" }}
+        editor={editor}
+      />
     </div>
   );
 };
